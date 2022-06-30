eeb3b848811d4b861bd03fb80f1488987990421f
Denis Svetlakov <dps150482@gmail.com>
2022-06-30 22:32:33 +1000

    Initial Commit

---
+++ Football/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football
+//
+//  Created by Denis Svetlakov on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
